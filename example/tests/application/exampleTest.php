1c56ab66d675c8b63aab89264be1822ef71857fe
Marco Patiño <mark@Marks-MacBook-Pro.local>
2013-09-20 20:46:58 -0700

    Example of phpunit basic setup

---
+++ example/tests/application/exampleTest.php
@@ -0,0 +1,9 @@
+<?php
+
+class ExampleTest extends PHPUnit_Framework_TestCase
+{
+    public function test()
+    {
+        $this->assertTrue(true);
+    }
+}
---
+++ example/tests/bootstrap.php
@@ -0,0 +1,7 @@
+<?php
+/*
+set_include_path(implode(PATH_SEPARATOR, array(
+    '/Users/mark/workshop/phpunit/example/vendor/phpunit/phpunit/PHPUnit',
+    get_include_path(),
+)));
+ */
